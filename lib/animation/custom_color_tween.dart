de76ee7bdb85ffc1bf90134f38be49312b92f86d
JustGinger888 <ethanmoffatt888@icloud.com>
2020-04-29 14:11:35 +0100

    cmt

---
+++ lib/animation/custom_color_tween.dart
@@ -0,0 +1,18 @@
+//import 'package:flutter/material.dart';
+//
+//class CustomColorTween {
+//  AnimationController controller;
+//  Animation animation;
+//
+//controller =
+//AnimationController(duration: Duration(seconds: 1), vsync: this);
+//animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+//    .animate(controller);
+//controller.forward();
+//controller.addListener(() {
+//setState(() {
+//print(animation.value);
+//});
+//});
+//
+//}
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,11 +1,23 @@
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flash_chat/components/login_reg_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../constants.dart';
 
 class RegistrationScreen extends StatefulWidget {
+  static String id = 'regScreen';
+
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,80 +28,54 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: 200.0,
-              child: Image.asset('images/logo.png'),
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
             ),
             SizedBox(
               height: 48.0,
             ),
             TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-            ),
+              keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kLDeooTextBox.copyWith(
+                  hintText: 'Enter Your Email',
+                )),
             SizedBox(
               height: 8.0,
             ),
             TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-            ),
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kLDeooTextBox.copyWith(
+                  hintText: 'Enter Your Password',
+                )),
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement registration functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
+            LoginRegButton(
+                buttonColor: Colors.blueAccent,
+                onPress: () async{
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    if(newUser != null){
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  }catch(e) {
+                    print(e);
+                  }
+                },
+                buttonText: 'Register'),
           ],
         ),
       ),
